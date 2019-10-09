1fe33a6e956796ed5aa2a7294ee7883bb88744b6
Joshua <jcooler.cooler@gmail.com>
2019-10-09 10:11:14 -0700

    Update drawLinesBetween.js
    updated the component to draw a line to the next object however they do not dissapear and it will need easing if we want it to look ok.

--- scripts/components/drawLinesBetween.js
+++ scripts/components/drawLinesBetween.js
@@ -8,27 +8,29 @@ AFRAME.registerComponent("drawlinesbetween", {
     let data = this.data;
     let el = this.el;
     let currentGeneratedObject =
-      gameState.generatedObjects[gameState.currentShownNum];
-    let lastGeneratedObject =
       gameState.generatedObjects[gameState.currentShownNum - 1];
+    let lastGeneratedObject =
+      gameState.generatedObjects[gameState.currentShownNum - 2];
     if (
       el.sceneEl.contains(currentGeneratedObject) &&
-      el.sceneEl.contains(lastGeneratedObject)
+      el.sceneEl.contains(lastGeneratedObject) &&
+      currentGeneratedObject.object3D.position.z < 0
     ) {
       let lineEntity = document.createElement("a-entity");
       lineEntity.setAttribute("line", {
         start: {
           x: lastGeneratedObject.object3D.position.x,
           y: lastGeneratedObject.object3D.position.y,
-          z: lastGeneratedObject.object3D.position.z
+          z: -30
         },
         end: {
           x: currentGeneratedObject.object3D.position.x,
           y: currentGeneratedObject.object3D.position.y,
-          z: currentGeneratedObject.object3D.position.z
+          z: -30
         },
         color: "red"
       });
+      el.sceneEl.appendChild(lineEntity);
     }
   }
 });
